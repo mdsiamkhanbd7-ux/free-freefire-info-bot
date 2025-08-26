09463e76599e1a8486d207900b58abe0ddc58d17
mdsiamkhanbd7-ux <md.siamkhan.bd7@gmail.com>
2025-08-27 01:42:09 +0600

    Update infoCommands.py

--- cogs/infoCommands.py
+++ cogs/infoCommands.py
@@ -257,7 +257,7 @@ class InfoCommands(commands.Cog):
 
 
 
-            embed.set_footer(text="DEVELOPED BY THUG")
+            embed.set_footer(text="Developed : ! 1n Only Leo")
             await ctx.send(embed=embed)
 
             if region and uid:
